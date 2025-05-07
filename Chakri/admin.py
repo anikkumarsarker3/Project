09858a71c804474a1b45081f93d50ee0a62591e1
Anik Kumar <77073795+anikkumarsarker3@users.noreply.github.com>
2025-05-07 07:04:28 +0600

    Delete Chakri directory

--- Chakri/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile, Job, Application
-# Register your models here.
-admin.site.register(UserProfile)
-admin.site.register(Job)
-admin.site.register(Application)
